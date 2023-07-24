b107b669de22c9f980281566d846404a952c867a
GOUNAYA Oussama <b179ry@login.axa>
2023-07-24 17:46:43 +0200

    feat(mongoDB): initialiser pour ajouter les users

---
+++ routes/authRoutes.js
@@ -0,0 +1,11 @@
+const passport = require('passport');
+
+module.exports = (app) => {
+    app.get('/auth/google', passport.authenticate(
+        'google', {
+          scope: ['profile', 'email'] // access user profile information such as name and email address
+        }
+      ))
+      
+    app.get('/auth/google/callback', passport.authenticate('google'));    
+}
