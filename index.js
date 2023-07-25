0780eae6a05eafb4a0da88e1e48d1deb2776cbab
GOUNAYA Oussama <b179ry@login.axa>
2023-07-25 23:49:33 +0200

    feat(mongo): controle the connection to mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys.js');
+
 require('./services/passport');
+mongoose.connect(keys.mongoURI)
+    .then(() => console.log('Connected!'))
+    .catch(()=> console.log('Not Connected'));
 
 const app = express();
+
 require('./routes/authRoutes')(app);
 
 
