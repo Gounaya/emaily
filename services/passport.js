27168b4d456468d6cb2b516dc0e6fc302c49dcc5
GOUNAYA Oussama <b179ry@login.axa>
2023-08-12 15:05:06 +0200

    serialize and deserialise user

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,15 @@ const keys = require('../config/keys.js');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => done(null, user))
+})
+
 // https://console.cloud.google.com/
 passport.use(
     new GoogleStrategy({
