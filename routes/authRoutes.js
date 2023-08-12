0aad15a14c3cb05ab8e90fa1ca18a531120054b6
GOUNAYA Oussama <b179ry@login.axa>
2023-08-13 00:34:04 +0200

    logging out user from the app

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,11 @@ module.exports = (app) => {
     // Auth with Google
     app.get('/auth/google/callback', passport.authenticate('google')); 
 
+    app.get('/api/logout', (req, res) => {
+      req.logout();
+      res.send(req.user)
+    })
+
     // Testing authentication 
     app.get('/api/current_user', (req, res) => {
       res.send(req.user);
